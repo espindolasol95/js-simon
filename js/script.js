bcd3601d0695fb17d559db38a235c363b76c3010
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-11 17:44:39 +0200

    feat (dom, number e countdown)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// vado a recuperare gli elementi del DOM che mi servono 
+
+const countdown= document.getElementById('countdown');
+const numberList= document.getElementById('numberList');
+const instructions= document.getElementById ('instructions');
+const answersForm= document .getElementById ('answersForm');
+const message=document.getElementById('messagge');
+const inputGroup= document.getElementById ('inputGroup');
+
+const totalNumbers= 5;
+const countdown= 30;
+
+
+let generateNumbers = [];
