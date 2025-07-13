fedad430ccfcd6b33a7172f2d6f369a5a5e18b88
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-13 19:12:21 +0200

    feat (generatore di numeri random)

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 // vado a recuperare gli elementi del DOM che mi servono 
 
 const countdown= document.getElementById('countdown');
-const numberList= document.getElementById('numberList');
+const numberList= document.getElementById('number-list');
 const instructions= document.getElementById ('instructions');
 const answersForm= document .getElementById ('answersForm');
-const message=document.getElementById('messagge');
+const messagge=document.getElementById('messagge');
 const inputGroup= document.getElementById ('inputGroup');
 
 const totalNumbers= 5;
 const countdownSeconds= 30;
-
-
 let generateNumbers = [];
+// generatore di numeri random
+function generateRandomNumbers(total , min = 1, max=50){
+    const range= [];
+    for (let i = min; i <= max; i ++){
+        range.push (i);
+    }
+}    
 
+
+    for (let i = range.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [range[i], range[j]] = [range[j], range[i]];
+    }
